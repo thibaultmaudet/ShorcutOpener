fa1dba34d7cb859b2583b7956690ade30aa686c3
Thibault MAUDET <41793338+thibaultmaudet@users.noreply.github.com>
2020-10-08 18:54:45 +0200

    Added internal commands to add new shorcuts and reload shorcuts list.

--- src/ShorcutOpener/ViewModels/ShellViewModel.cs
+++ src/ShorcutOpener/ViewModels/ShellViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Windows;
 using System.Windows.Input;
 using Dev2Be.Toolkit;
 using ShorcutOpener.Core.Models;
@@ -16,6 +17,8 @@ namespace ShorcutOpener.ViewModels
         private string text;
 
         private List<Shorcut> shorcuts;
+
+        private readonly List<string> internalCommands;
 
         public ICommand IgnoreCommand { get; set; }
 
@@ -32,9 +35,9 @@ namespace ShorcutOpener.ViewModels
         {
             InitializeKeyCommands();
 
-            AssemblyInformations assemblyInformations = new AssemblyInformations(Assembly.GetExecutingAssembly().GetName().Name);
+            shorcuts = Reload();
 
-            shorcuts = new FileService().Read<List<Shorcut>>(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), assemblyInformations.Company, assemblyInformations.Product), "ShorcutOpener.Shorcuts.json");
+            internalCommands = new List<string>() { "edit", "exit", "reload" };
         }
 
         private void InitializeKeyCommands()
@@ -48,14 +51,44 @@ namespace ShorcutOpener.ViewModels
 
             ValidateCommand = new RelayCommand<object>(_ =>
             {
+                Shorcut shorcut = null;
+
                 if (text.Length > 0 && shorcuts.Count > 0)
                 {
-                    Shorcut shorcut = shorcuts.Find(x => x.Text.Equals(text, StringComparison.InvariantCultureIgnoreCase));
+                    shorcut = shorcuts.Find(x => x.Text.Equals(text, StringComparison.InvariantCultureIgnoreCase));
 
                     if (shorcut != null)
                         Process.Start(new ProcessStartInfo(shorcut.Process) { UseShellExecute = true, Arguments = shorcut.Argument });
                 }
+                
+                if (text.Length > 0 && shorcut == null)
+                {
+                    string command = internalCommands.Find(x => x.Equals(text, StringComparison.InvariantCultureIgnoreCase));
+
+                    switch (command)
+                    {
+                        case "edit":
+                            AssemblyInformations assemblyInformations = new AssemblyInformations(Assembly.GetExecutingAssembly().GetName().Name);
+                            Process.Start(new ProcessStartInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), assemblyInformations.Company, assemblyInformations.Product, "ShorcutOpener.Shorcuts.json")) { UseShellExecute = true });
+                            break;
+                        case "exit":
+                            Application.Current.Shutdown();
+                            break;
+                        case "reload":
+                            shorcuts = Reload();
+                            break;
+                        default:
+                            break;
+                    }
+                }
             });
+        }
+
+        private List<Shorcut> Reload()
+        {
+            AssemblyInformations assemblyInformations = new AssemblyInformations(Assembly.GetExecutingAssembly().GetName().Name);
+
+            return new FileService().Read<List<Shorcut>>(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), assemblyInformations.Company, assemblyInformations.Product), "ShorcutOpener.Shorcuts.json");
         }
     }
 }
